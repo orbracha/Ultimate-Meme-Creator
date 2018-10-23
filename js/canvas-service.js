c8a189944e2d85afe28cabd552a44c90acc26429
BeBarefoot <35373216+BeBarefoot@users.noreply.github.com>
2018-10-23 13:20:57 +0300

    footer is on

---
+++ js/canvas-service.js
@@ -0,0 +1,6 @@
+'use strict'
+
+var gCanvas = document.querySelector('.canvas')
+var gCtx = gCanvas.getContext('2d')
+
+gCanvas.style.border = '1px solid black'
---
+++ js/text-service.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function openTextEditor(){
+    $('.main-footer-controls').hide()
+    $('.main-text-controls').show()
+}
+function backToMainControls(){
+    $('.main-footer-controls').show()
+    $('.main-text-controls').hide()
+}
